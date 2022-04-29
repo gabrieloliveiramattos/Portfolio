c19c3fc7354c5701a193ae771616a2af5944a954
Gabriel Oliveira de Matos <42393018+gabrieloliveiramattos@users.noreply.github.com>
2022-04-29 16:19:50 -0300

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -186,6 +186,88 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-icon' , getCurrentIcon())
 })
 
+/*==================== MESSAGE ====================*/ 
+
 function alertMenssage(){
     alert('No momento esse botão encontra-se desativado, por favor entre em contato pelo Linkedin\nTelefone ou e-mail');
 }
+
+/*==================== COLOR THEME ====================*/
+const colorButton = document.getElementById('change-color')
+const colorTheme = getComputedStyle(document.documentElement).getPropertyValue('--hue-color')
+
+const selectedColor = localStorage.getItem('selected-color')
+
+const getCurrentColor = () => colorTheme
+
+if (selectedColor){
+    colorButton.setAttribute('Value', hslToHex(selectedColor,69,61))
+    document.documentElement.style.setProperty('--hue-color', selectedColor)
+}
+
+colorButton.addEventListener('change', () => {
+    document.documentElement.style.setProperty('--hue-color', hexToHSL(colorButton.value))
+
+    localStorage.setItem('selected-color' , hexToHSL(colorButton.value))
+})
+
+/*==================== HSL TO HEX ====================*/
+
+function hslToHex(h, s, l) {
+    h /= 360;
+    s /= 100;
+    l /= 100;
+    let r, g, b;
+    if (s === 0) {
+      r = g = b = l; // achromatic
+    } else {
+      const hue2rgb = (p, q, t) => {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1 / 6) return p + (q - p) * 6 * t;
+        if (t < 1 / 2) return q;
+        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
+        return p;
+      };
+      const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      const p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1 / 3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1 / 3);
+    }
+    const toHex = x => {
+      const hex = Math.round(x * 255).toString(16);
+      return hex.length === 1 ? '0' + hex : hex;
+    };
+    return `#${toHex(r)}${toHex(g)}${toHex(b)}`;
+}
+
+/*==================== HEX TO HSL ====================*/
+
+function hexToHSL(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      r = parseInt(result[1], 16);
+      g = parseInt(result[2], 16);
+      b = parseInt(result[3], 16);
+      r /= 255, g /= 255, b /= 255;
+      var max = Math.max(r, g, b), min = Math.min(r, g, b);
+      var h, s, l = (max + min) / 2;
+      if(max == min){
+        h = s = 0; // achromatic
+      }else{
+        var d = max - min;
+        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+        switch(max){
+          case r: h = (g - b) / d + (g < b ? 6 : 0); break;
+          case g: h = (b - r) / d + 2; break;
+          case b: h = (r - g) / d + 4; break;
+        }
+        h /= 6;
+      }
+      return Math.round(h * 360)
+    var HSL = new Object();
+    HSL['h']=h;
+    HSL['s']=s;
+    HSL['l']=l;
+    return HSL;
+  }
